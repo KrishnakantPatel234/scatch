a097306f342a037b96ae54b702a1e6b564d84757
Krishna <kp8353156@gmail.com>
2026-03-04 16:49:52 +0530

    Secured Create route for owner only

--- config/index.js
+++ config/index.js
@@ -1,13 +1,35 @@
 import mongoose from "mongoose";
+import debug from "debug";
+import config from "config";
+
+const dblog = debug("development:mongoose");
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("mongodb connected successfully....");
+        await mongoose.connect(`${config.get("MONGODB_URI")}/scatch-db`);
+
+        dblog("mongodb connected successfully....");
+        console.log("connectDB function running");
     }catch(err){
-        console.log("caught an error : " , err);
+        dblog("MongoDB connection error: %O", err);
         process.exit(1);
     }
 };
 
 export default connectDB;
+
+// USING DOTENV
+
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//     try{
+//         await mongoose.connect(process.env.MONGODB_URI);
+//         console.log("mongodb connected successfully....");
+//     }catch(err){
+//         console.log("caught an error : " , err);
+//         process.exit(1);
+//     }
+// };
+
+// export default connectDB;
--- routes/owners.routes.js
+++ routes/owners.routes.js
@@ -1,9 +1,30 @@
 import {Router} from "express";
+import Owner from "../models/owner.models.js";
 
 const router = Router();
 
 router.get("/" , (req , res)=> {
     res.send("Hey");
 })
+
+if(process.env.NODE_ENV === "development"){
+    router.post("/create" , async (req , res)=> {
+        const owner = await Owner.find();
+
+        if(owner.length > 0){
+            return res.status(500).send("You are not allowed to create a new Owner.");
+        }
+
+        const {email , fullname , password} = req.body;
+
+        const createdOwner = await Owner.create({
+            fullname,
+            email,
+            password
+        });
+
+        res.status(201).send(createdOwner);
+    })
+}
 
 export default router;
